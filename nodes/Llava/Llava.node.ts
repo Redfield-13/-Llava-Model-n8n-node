ae2dd2ee4cbe9475f9bd13d85a294a1f22abb9a0
unknown <fieldr62@gmail.com>
2024-03-04 22:39:01 -0800

    files/folders names changes

--- nodes/Llava/Llava.node.ts
+++ nodes/Llava/Llava.node.ts
@@ -4,16 +4,16 @@ import {
 	IDataObject,INodeExecutionData,INodeType,INodeTypeDescription,} from 'n8n-workflow';
 
 // Main Class
-export class Trace implements INodeType {
+export class Llava implements INodeType {
     description: INodeTypeDescription = {
       displayName: 'Ask LLAVA',
-      name: 'Trace',
+      name: 'Llava',
       icon: 'file:exchangeRate.svg',
       group: ['input'],
       version: 1,
       description: 'Ask our model LLAVA about anythin in any image.',
       defaults: {
-        name: 'Trace',
+        name: 'Llava',
       },
     inputs: ['main'],
     outputs: ['main'],
@@ -54,7 +54,7 @@ export class Trace implements INodeType {
             const imageUrl = this.getNodeParameter('imageUrl',i) as string;
             const prompt = this.getNodeParameter('prompt',i) as string;
 
-            let Url = `http://127.0.0.1:8000/imagedesc?url=${imageUrl}&prompt=${prompt}`;
+            let Url = `http://127.0.0.1:8000/llava?url=${imageUrl}&prompt=${prompt}`;
             let response = await this.helpers.request({ method: 'GET', url: Url, json: true });
             let trace = response
             responseData.push({Url, trace})
